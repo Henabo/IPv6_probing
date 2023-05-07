931f7aa3fc0b699e56cce7141e76d75a0af4d98b
DressPD <arisdressino@gmail.com>
2023-05-07 23:28:11 +0200

    entropy-ip and python envs

--- retrieve_addresses.sh
+++ retrieve_addresses.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# run script to retrieve IPv6 addresses and store in local directory
+#conda activate py3_env && \
 python ./ipv6_retrieval/ipv6_retrieval.py
+
+# run script to identify aliased addresses and create alias using predefine algorithm
 python ./ipv6_retrieval/ipv6_identify_prefixes.py
+
+# run zmapv6 scan in order to select de-aliased IPv6 addresses and store them
+# WIP
