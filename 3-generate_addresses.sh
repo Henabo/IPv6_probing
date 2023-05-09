7d1708e935ea816a9a2fe6670303efea53ba2412
DressPD <arisdressino@gmail.com>
2023-05-09 02:09:21 +0200

    update bash scripts ordering

--- 3-generate_addresses.sh
+++ 3-generate_addresses.sh
@@ -4,7 +4,7 @@
 #conda activate py3_env && \
 python ./ipv6_retrieval/ipv6_transform.py > transformed_ipv6_addresses.txt
 
-# run script to generate new IPv6 addresses and store them in a file
+# run script to generate Bayesian Modeling of de-aliased IPv6 addresses for addresses' generation
 #conda activate py2_env && \
 cd entropy-ip && \
 ./ALL.sh ../transformed_ipv6_addresses.txt ../generated_ipv6_addresses
@@ -12,6 +12,9 @@ cd entropy-ip && \
 # go back to working directory
 cd ..
 #conda deactivate
+
+# run script to generate new IPv6 addresses based on previous modeling and store them in a file
+# WIP with eip-generator
 
 # scan them for one week and generate report of results
 # WIP
